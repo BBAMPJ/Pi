881d2f0342c8228a0ca1260f2a25edf07b8753a9
BBAMPJ <92916352+BBAMPJ@users.noreply.github.com>
2024-02-26 22:07:16 -0800

    Create Lab9_3.py

---
+++ Lab9_3.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+from flask import Flask, render_template
+
+LED1  = 11  
+LED2  = 13
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(LED1, GPIO.OUT)
+GPIO.setup(LED2, GPIO.OUT)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')w
+
+@app.route('/<led>/<st>')
+def control(led, st):
+    if led == 'led1':
+        if st == 'on':
+            GPIO.output(LED1, 1)
+            return f'Led 1 - ON'
+        if st == 'off':
+            GPIO.output(LED1, 0)
+            return f'Led 1 - OFF'
+    elif led == 'led2':
+        if st == 'on':
+            GPIO.output(LED2, 1)
+            return f'Led 2 - ON'
+        if st == 'off':
+            GPIO.output(LED2, 0)
+            return f'Led 2 - OFF'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='192.168.204.172')
